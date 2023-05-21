5f61a2ccc52a16fbd754399e052e815a120cd7bc
Kali <gkvp70@gmail.com>
2023-05-21 06:22:15 +0530

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from '../home/home.component';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    HomeComponent
+  ]
+})
+export class SharedModule { }
